08cd8b3ab240d55896b88e47af601915a970fff4
jose.acosta <jose.acosta@tutenlabs.com>
2023-10-29 22:23:39 -0300

    add dark mode and optimize the js source

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 /* Copyright José Acosta. All Rights Reserved. */
+
+/* navigation */
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+/* deafult yeard */
+var currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+/* dark mode switch */
+const darkModeSwitch = document.getElementById('toggle_checkbox');
+const body = document.body;
+
+darkModeSwitch.addEventListener('change', function() {
+body.classList.toggle('dark-mode');
+});
